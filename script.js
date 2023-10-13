58aa11e9aabfc69e1f0a00c41307cd828c277d06
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-10-13 17:05:05 +0800

    Add event listener to div; move factory function

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const gameBoard = (() => {
 
     const renderBoard = () => {
         const board = document.querySelector(".board");
-
-        gameBoard.forEach(() => {
-            const cell = document.createElement("div");
-            cell.classList.add("square");
-            board.appendChild(cell);
+        let boardHTML = '';
+        
+        gameBoard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
         })
+        board.innerHTML = boardHTML;
+        const square = document.querySelectorAll(".square");
+
+        square.forEach((square) => square.addEventListener("click", () => {
+            square.textContent = gamePlay.player[0].playerToken;
+        }))
     }
 
     return {
@@ -16,10 +21,41 @@ const gameBoard = (() => {
     }
 })();
 
-gameBoard.renderBoard();
+const addPlayer = (name, token) => {
+    const playerName = name;
+    const playerToken = token;
+    return {
+        playerName, playerToken
+    }
+};
+
+const gamePlay = (() => {
+    const playerOne = document.querySelector("#playerOne").value;
+    const playerTwo = document.querySelector("#playerTwo").value;
+    let currentPlayer;
+    let gameOver;
+
+    let player = [
+        addPlayer(playerOne, "X"),
+        addPlayer(playerTwo, "O")
+    ]
+
+    const start = () => {
+        currentPlayer = player[0];
+        gameOver = false;
+        gameBoard.renderBoard();
+    }
+
+
+
+    return {
+        start, player, firstPlayer
+    }
+})();
 
 const startBtn = document.querySelector("#start");
 
-startBtn.addEventListener("click", () => {
-    // gamePlay.start();
+startBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    gamePlay.start();
 })
