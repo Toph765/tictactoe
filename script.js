70299529a724d400ea5486bb886e3973aa752fff
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-10-16 15:08:58 +0800

    Add reset function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    const gameBoard = ["", "", "", "", "", "", "", "", ""];
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
 
     const getBoard = () => gameBoard;
 
@@ -10,17 +10,15 @@ const gameBoard = (() => {
         let boardHTML = '';
         
         gameBoard.forEach((square, index) => {
-            boardHTML += `<button class="square style" id="square-${index}">${square}</button>`;
+            boardHTML += `<button class="square" id="square-${index}">${square}</button>`;
         })
         board.innerHTML = boardHTML;
         const square = document.querySelectorAll(".square");
  
         square.forEach((cell, index) => cell.addEventListener("click", () => {
-
             if (cell.textContent) return;
-            cell.textContent = currentPlayer.playerToken;
             gameBoard[index] = currentPlayer.playerToken;
-            console.log(gameBoard)
+            cell.textContent = gameBoard[index];
 
             if (currentPlayer === player[0]) {
                 currentPlayer = player[1]
@@ -32,10 +30,11 @@ const gameBoard = (() => {
 
             if (gamePlay.getGameOver()) {
                 square.forEach((square) => square.setAttribute("disabled", ""));
+                gameBoard = ["", "", "", "", "", "", "", "", ""];
             }
         }))
     }
-
+    
     return {
         renderBoard, getBoard
     }
@@ -52,7 +51,6 @@ const addPlayer = (name, token) => {
 const gamePlay = (() => {
     const playerOne = document.querySelector("#playerOne").value;
     const playerTwo = document.querySelector("#playerTwo").value;
-    const board = gameBoard.getBoard();
     let currentPlayer;
     let gameOver;
 
@@ -64,34 +62,42 @@ const gamePlay = (() => {
         addPlayer(playerTwo, "O")
     ]
 
+    const winCondition = () => {
+        let board = gameBoard.getBoard();
+        
+        if ((board[0] === board[1] && board[0] === board[2] && (board[0] === "X" || board[0] === "O")) ||
+            (board[3] === board[4] && board[3] === board[5] && (board[3] === "X" || board[3] === "O")) ||
+            (board[6] === board[7] && board[6] === board[8] && (board[6] === "X" || board[6] === "O")) ||
+            (board[0] === board[4] && board[0] === board[8] && (board[0] === "X" || board[0] === "O")) ||
+            (board[2] === board[4] && board[2] === board[6] && (board[2] === "X" || board[2] === "O")) ||
+            (board[0] === board[3] && board[0] === board[6] && (board[0] === "X" || board[0] === "O")) ||
+            (board[1] === board[4] && board[1] === board[7] && (board[1] === "X" || board[1] === "O")) ||
+            (board[2] === board[5] && board[2] === board[8] && (board[2] === "X" || board[2] === "O"))) {
+                alert("Game Over!");
+                gameOver = true;
+                board = board;
+            } else return;
+    }
+
     const start = () => {
         currentPlayer = player[0];
         gameOver = false;
         gameBoard.renderBoard();
     }
 
-    const winCondition = () => {
-        if ((board[0] === board[1] && board[1] === board[2] && board[0] === board[2] && (board[0] === "X" || board[0] === "O")) ||
-            (board[3] === board[4] && board[4] === board[5] && board[3] === board[5] && (board[3] === "X" || board[3] === "O")) ||
-            (board[6] === board[7] && board[7] === board[8] && board[6] === board[8] && (board[6] === "X" || board[6] === "O")) ||
-            (board[0] === board[4] && board[4] === board[8] && board[0] === board[8] && (board[0] === "X" || board[0] === "O")) ||
-            (board[2] === board[4] && board[4] === board[6] && board[2] === board[6] && (board[2] === "X" || board[2] === "O")) ||
-            (board[0] === board[3] && board[3] === board[6] && board[0] === board[6] && (board[0] === "X" || board[0] === "O")) ||
-            (board[1] === board[4] && board[4] === board[7] && board[1] === board[7] && (board[1] === "X" || board[1] === "O")) ||
-            (board[2] === board[5] && board[5] === board[8] && board[2] === board[8] && (board[2] === "X" || board[2] === "O"))) {
-                alert("Game Over!");
-                gameOver = true;
-            } else return;
-    }
-
     return {
-        start, player, getPlayer,getGameOver, winCondition
+        start, reset, player, getPlayer,getGameOver, winCondition,
     }
 })();
 
 const startBtn = document.querySelector("#start");
+const resetBtn = document.querySelector("#reset");
 
 startBtn.addEventListener("click", (e) => {
     e.preventDefault();
     gamePlay.start();
-})
+});
+
+resetBtn.addEventListener("click", (e) => {
+    gamePlay.start();
+});
